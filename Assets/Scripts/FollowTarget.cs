f4c14a32665196cbb83ee31c03c84360247f4754
saporter <scott.porter.ii@gmail.com>
2020-05-27 20:26:42 -0500

    Sprite Test

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform Target;
+    
+    protected virtual void LateUpdate()
+    {
+        transform.position = new Vector3(Target.position.x, Target.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    protected float _horizontal;
+    protected float _vertical;
+
+    protected virtual void FixedUpdate()
+    {
+        var newPos = transform.position;
+        newPos.x = newPos.x + _horizontal * Speed * Time.deltaTime;
+        newPos.y = newPos.y + _vertical * Speed * Time.deltaTime;
+        transform.position = newPos;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+}
