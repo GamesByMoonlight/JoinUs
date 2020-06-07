32524e33e1f3a2b40fec6a1e15724666a96b516c
Albangalo <Albert.chen.nz@gmail.com>
2020-06-08 00:16:21 +1200

    Repitching works

---
+++ Assets/TouchRepitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRepitcher : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Repitcher")
+        {
+            var repitcher = collision.gameObject.GetComponent<Repitcher>();
+            AudioEvents.current.PitchTrigger(repitcher.pitch);
+            print("triggering repitch event");
+        }
+    }
+}
