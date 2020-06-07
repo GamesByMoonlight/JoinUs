764da6d6dc7e34676c44ac0185e43914223b6201
Albangalo <Albert.chen.nz@gmail.com>
2020-06-08 11:33:17 +1200

    toggle bars volume

---
+++ Assets/AudioToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioToggle : MonoBehaviour
+{
+    private bool playing = true;
+    public AudioMixer mixer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("triggered by " + collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player")
+        {
+            if (playing)
+            {
+                mixer.SetFloat(tag, -10f);
+                playing = false;
+            }
+            else
+            {
+                mixer.SetFloat(tag, -0);
+                playing = true;
+            }
+        }
+    }
+}
