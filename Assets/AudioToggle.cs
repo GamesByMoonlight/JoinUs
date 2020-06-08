5e9a4a44432e63e423bd5c081fc1dbe4ccf08782
Albert Chen <Albert.chen.nz@gmail.com>
2020-06-08 10:31:52 -0400

    Musical tiles (#2)
    * created and imported audio loops
    
    * Repitching works
    
    * Replaced Event based Repitching with AudioMixer
    
    * toggle bars volume
    
    * Added other instruments
    
    * simplified audio toggle logic

---
+++ Assets/AudioToggle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioToggle : MonoBehaviour
+{
+    private const float QUIET_VOLUME = -10f;
+    private float volume = 0;
+    public AudioMixer mixer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // toggles volume of audio group between loud and quiet
+        if (collision.gameObject.tag == "Player")
+        {
+            volume = QUIET_VOLUME - volume;
+            mixer.SetFloat(tag, volume);
+        }
+    }
+}
---
+++ Assets/Repitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repitcher : MonoBehaviour
+{
+    public float pitch = 1.333f;
+
+    public UnityEngine.Audio.AudioMixer mixer;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            mixer.SetFloat("Pitch", pitch);
+        }
+    }
+}
