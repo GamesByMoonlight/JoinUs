83ab398fce2d4018ef5e31a7698d70dd08c19d98
Albangalo <Albert.chen.nz@gmail.com>
2020-06-08 13:51:42 +1200

    simplified audio toggle logic

--- Assets/AudioToggle.cs
+++ Assets/AudioToggle.cs
@@ -1,29 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 
 public class AudioToggle : MonoBehaviour
 {
     private const float QUIET_VOLUME = -10f;
-    private const float NORMAL_VOLUME = 0;
-    private bool playing = true;
+    private float volume = 0;
     public AudioMixer mixer;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // toggles volume of audio group between loud and quiet
         if (collision.gameObject.tag == "Player")
         {
-            if (playing)
-            {
-                mixer.SetFloat(tag, QUIET_VOLUME);
-                playing = false;
-            }
-            else
-            {
-                mixer.SetFloat(tag, NORMAL_VOLUME);
-                playing = true;
-            }
+            volume = QUIET_VOLUME - volume;
+            mixer.SetFloat(tag, volume);
         }
     }
 }
