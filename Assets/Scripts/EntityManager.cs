ccd004c7145972ab804eb5b42e0fa8c4b6866560
amf7619 <42848951+amf7619@users.noreply.github.com>
2020-06-12 11:03:16 -0400

    Zombies and NPC's (#5)
    A Ferraioli's contributions 
    
    Added Zombies that will chase the player and NPCs. They will also try to not pile on top of each other.
    Added NPCs that run from zombies. Will also follow the player when the player interacts with them.
    Added gravestones for zombie spawning.

---
+++ Assets/Scripts/EntityManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Keeps track of the entities in the field and initiates spawning
+/// </summary>
+public class EntityManager : MonoBehaviour
+{
+    public GameObject zombiePrefab;
+    public GameObject npcPrefab;
+
+    public GameObject player;
+    public List<GameObject> gravestones;
+    private List<GameObject> npcs;
+    private List<GameObject> zombies;
+
+    public int NPCsSpawned = 5;
+    public int percentChanceOfSpawn = 50;
+    public int maxZombies = 10;
+
+    private float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        npcs = new List<GameObject>();
+        zombies = new List<GameObject>();
+
+        elapsedTime = 0f;
+
+        SpawnNPCs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //code calls SpawnZombie once every second instead of every frame
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime >= 1f)
+        {
+            elapsedTime %= 1f;
+            SpawnZombie();
+        }
+    }
+
+    void SpawnNPCs()
+    {
+        for(int i = 0; i < NPCsSpawned; i++)
+        {
+            float x = Random.Range(-15, 15);
+            float y = Random.Range(-15, 15);
+
+            npcs.Add(Instantiate(npcPrefab, new Vector3(x, y), Quaternion.identity));
+        }
+    }
+
+    void SpawnZombie()
+    {
+        int randNum = Random.Range(1, 100);
+        if(randNum <= percentChanceOfSpawn)
+        {
+            int index = Random.Range(0, gravestones.Count);
+            GameObject newZombie = Instantiate(zombiePrefab, gravestones[index].transform.position, Quaternion.identity);
+            zombies.Add(newZombie);
+
+            //maintain a max count of zombies in game
+            if(zombies.Count > maxZombies)
+            {
+                GameObject tempZomb = zombies[0];
+                zombies.RemoveAt(0);
+                Destroy(tempZomb);
+            }
+        }
+    }
+
+    public void NPCTurnedZombie(GameObject npc)
+    {
+        GameObject newZombie = Instantiate(zombiePrefab, npc.transform.position, Quaternion.identity);
+        zombies.Add(newZombie);
+        npcs.Remove(npc);
+    }
+
+    public void ZombieTurnedNPC(GameObject zombie)
+    {
+        GameObject newNPC = Instantiate(npcPrefab, zombie.transform.position, Quaternion.identity);
+        npcs.Add(newNPC);
+        zombies.Remove(zombie);
+    }
+
+    public List<GameObject> GetNPCs()
+    {
+        return npcs;
+    }
+
+    public List<GameObject> GetZombies()
+    {
+        return zombies;
+    }
+
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+}
---
+++ Assets/Scripts/NPCLogic.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCLogic : MonoBehaviour
+{
+    private EntityManager entityManager;
+
+    public float viewRange = 15f;
+    public float movementSpeed = 5f;
+
+    private Vector3 followVector;
+    private float followWeight = 1f;
+    private Vector3 runVector;
+    private float runWeight = 3f;
+
+    public GameObject turnedPrefab;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entityManager = GameObject.Find("EntityManager").GetComponent<EntityManager>();
+        player = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+        Run();
+        Move();
+    }
+
+    void Run()
+    {
+        Vector3 dir = Vector3.zero;
+        foreach (GameObject z in entityManager.GetZombies())
+        {
+            if(z == null)
+            {
+                continue;
+            }
+
+            float dist = DistanceFrom(z);
+            if (dist <= viewRange / 2)
+            {
+                Vector3 nPos = transform.position;
+                Vector3 zPos = z.transform.position;
+                Vector3 direction = nPos - zPos;
+                dir += direction / dist;
+            }
+        }
+        runVector = dir.normalized * movementSpeed;
+    }
+
+    float DistanceFrom(GameObject g)
+    {
+        Vector3 gPos = g.transform.position;
+        Vector3 zPos = transform.position;
+        float hypot = Mathf.Sqrt(Mathf.Pow(gPos.x - zPos.x, 2) + Mathf.Pow(gPos.y - zPos.y, 2));
+        return hypot;
+    }
+
+    void Follow()
+    {
+        followVector = Vector3.zero;
+        if(player && DistanceFrom(player) < viewRange && DistanceFrom(player) > 1)
+        {
+            Vector3 nPos = transform.position;
+            Vector3 pPos = player.transform.position;
+            Vector3 movement = new Vector3(pPos.x - nPos.x, pPos.y - nPos.y);
+            followVector = movement.normalized;
+        }
+    }
+
+    void Move()
+    {
+        Vector3 moveVector = (followVector * followWeight) + (runVector * runWeight);
+        transform.position += moveVector.normalized * movementSpeed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Zombie")
+        {
+            entityManager.NPCTurnedZombie(gameObject);
+            GameObject effect = Instantiate(turnedPrefab, collision.transform.position, Quaternion.identity) as GameObject;
+            Destroy(effect.gameObject, 2.0f);
+            Destroy(gameObject);
+            Debug.Log(entityManager.GetZombies());
+            Debug.Log(entityManager.GetNPCs());
+        }
+
+        if (collision.transform.tag == "Player")
+        {
+            player = collision.gameObject;
+        }
+    }
+}
