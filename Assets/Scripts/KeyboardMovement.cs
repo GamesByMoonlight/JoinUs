507a9025a903ff72ed663f1ecc96689274741e67
wickedManatee <wicked.manatee.games@gmail.com>
2020-06-12 21:31:34 -0500

    Player animation and attack
    - added sprite sheet for player animation and movement controller (this part could be ported to Scoundrels with a better spritesheet than what I did)
    - added basic attack for player that kills zombies because they were super annoying

--- Assets/Scripts/KeyboardMovement.cs
+++ Assets/Scripts/KeyboardMovement.cs
@@ -10,6 +10,14 @@ public class KeyboardMovement : MonoBehaviour
     protected float _horizontal;
     protected float _vertical;
 
+    protected Animator animator;
+
+    protected virtual void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        Assert.IsTrue(animator);
+    }
+
     protected virtual void FixedUpdate()
     {
         var newPos = transform.position;
@@ -23,5 +31,9 @@ public class KeyboardMovement : MonoBehaviour
     {
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", _horizontal);
+        animator.SetFloat("Vertical", _vertical);
+        animator.SetFloat("Magnitude", new Vector3(_horizontal, _vertical, 0f).magnitude);
     }
 }
