60a5a5362e3de315036b99814dcc15f994131328
Silvia <suiqingc@andrew.cmu.edu>
2020-06-15 08:26:47 -0400

    created simple speed up skill, added a dash sprite

---
+++ Assets/Scripts/SpeedUpSkill.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpSkill : MonoBehaviour
+{
+    public float speedMultiplier = 1.2f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // set player as parent object (for skill retention) 
+            transform.SetParent(other.transform);
+
+            // turn off renderer after being 'absorbed' 
+            gameObject.GetComponentInChildren<Renderer>().enabled = false;
+
+            // turn on speed up skill 
+            SpeedPlayerUp();
+        }
+    }
+
+    private void SpeedPlayerUp()
+    {
+        // increase speed 
+        transform.parent.GetComponent<KeyboardMovement>().Speed *= speedMultiplier; 
+    }
+}
