66566da3ff89d8829e6a70fe7cc4fed1d78e5e52
Pixelmation <33030639+Pixelmation@users.noreply.github.com>
2020-06-15 13:00:50 -0400

    Added Capstone/Tile Count in Canvas (#12)
    * Added Capstone/Tile Count in Canvas
    
    Added a new Capstone Counter under controls. Call to change the text field is in TileFactory.cs with a public reference to the text object.
    
    * Added comments
    
    Added comments to TileFaactory.cs detailing my changes a bit better
    
    * Fixed merge issue with Silvia Chen
    
    Merge issue with user Silvia Chen should be fixed now
    
    Co-authored-by: Pixelmation <Pixelmation@users.noreply.github.com>

--- Assets/Scripts/TileFactory.cs
+++ Assets/Scripts/TileFactory.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TileFactory : MonoBehaviour
 {
     public static TileFactory Instance;
 
     public GameObject Prefab;
+    public AudioClip[] clips; //array of clips to play
+
+    //Text field from canvas
+    public Text capstoneCountText;
+    private int capstoneCountInt;
+
+    private AudioSource audioSource; //actually plays the clips
 
     protected virtual void Awake()
     {
         Instance = this;
+        audioSource = gameObject.GetComponent<AudioSource>();
+        capstoneCountInt = 1;
     }
 
     public virtual void Spawn(CornerstoneController controller)
     {
         Instantiate(Prefab, controller.transform.position, controller.transform.rotation);
+        audioSource.clip = clips[Random.Range(0, clips.Length)];
+        audioSource.Play();
+
+        //on tile creation add to count and display the new int
+        capstoneCountInt++;
+        capstoneCountText.text = capstoneCountInt.ToString();
     }
 }
